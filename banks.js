cba99582c825e10239a9dd6db4e8ae1ef2967c6c
manlikevee <odahviktor@gmail.com>
2022-09-18 22:15:48 +0100

    cgcgcc

--- banks.js
+++ banks.js
@@ -1,6 +1,14 @@
+const hideLoading = () => {
+  let overlay = document.getElementsByClassName('loading-overlay')[0]
+
+  // 👇️ hides element (still takes up space on page)
+  overlay.classList.remove('is-active')
+};
+
 // const accname = document.getElementById('accname');
 // const dog = document.createElement('p')
 const getAccountNumbers = () => {
+  displayLoading();
     fetch( `https://api.paystack.co/bank/` , {
         headers: {
             "Authorization" : "Bearer sk_test_d1b83ae902e7135db7d879d7121ec665e0a8243d" 
@@ -19,8 +27,11 @@ const getAccountNumbers = () => {
         }).join(" ");
         
         document.querySelector("#app").insertAdjacentHTML("afterbegin", html );
+        hideLoading();
+        
       }).catch(error => {
         alert(error)
+        hideLoading();
       } )
 
 
@@ -30,5 +41,9 @@ getAccountNumbers();
 
 
 
+const displayLoading = () => {
+  let overlay = document.getElementsByClassName('loading-overlay')[0]
+  overlay.classList.toggle('is-active')
+};
 
 
