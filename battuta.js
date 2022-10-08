75c7312a3ac0869ef365d2275cb9a190c1058f9d
manlikevee <odahviktor@gmail.com>
2022-10-08 12:21:27 +0100

    dc d

--- battuta.js
+++ battuta.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     //-------------------------------SELECT CASCADING-------------------------//
     var selectedCountry = (selectedRegion = selectedCity = "");
     // This is a demo API key for testing purposes. You should rather request your API key (free) from http://battuta.medunes.net/
-    var BATTUTA_KEY = "ebd4771475a538e302a0ffa08ca3898b";
+    var BATTUTA_KEY = "00000000000000000000000000000000";
     // Populate country select box from battuta API
     url =
       "https://battuta.medunes.net/api/country/all/?key=" +
